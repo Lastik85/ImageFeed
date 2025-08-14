97de3dc4ae523fa7fa2f9097b92f8292fb0c685c
Lastik85 <lastik198505@yandex.ru>
2025-08-15 00:18:30 +0300

    подправил код, убрал высоту(динамическая), зафиналил класс, минимальная версия 13.0

--- ImageFeed/ImageList/ImagesListViewController.swift
+++ ImageFeed/ImageList/ImagesListViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ImagesListViewController: UIViewController {
+final class ImagesListViewController: UIViewController {
 
     @IBOutlet private var tableView: UITableView!
 
@@ -17,7 +17,6 @@ class ImagesListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.rowHeight = 200
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
         
     }
