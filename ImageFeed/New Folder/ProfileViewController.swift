1150bcdf641b90f14983394c0f86800eaf7086b2
Lastik85 <lastik198505@yandex.ru>
2025-08-18 15:47:55 +0300

    экран профиля пользователя согласно дизайну, вроде)))

---
+++ ImageFeed/New Folder/ProfileViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+   @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+}
+
+
