eee0d7a69d029a7781e7ba1d9c3e0263726b0626
Lastik85 <lastik198505@yandex.ru>
2025-08-21 17:33:33 +0300

    закончил 9 спринт, тема Навигация

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+   @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var loginNameLable: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var exitButton: UIButton!
+    @IBAction private func didExitButton(){}
+    
+    
+}
+
+
