a1e89f0566456397fa2cc63b36ee08dcd9626085
Lastik85 <lastik198505@yandex.ru>
2025-09-28 13:27:26 +0300

    исправил secretKey

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -2,7 +2,7 @@ import UIKit
 
 enum Constants {
     static let accessKey = "N4eUXkAQZmOWXK6X2q1-MhtB4g9fOclbOx2yuJXAwGQ"
-    static let secretKey = "DtIEg4k7ddfwPLdhrJh0a5fmAJeh8FqBSs-6zfq7vMQ>"
+    static let secretKey = "DtIEg4k7ddfwPLdhrJh0a5fmAJeh8FqBSs-6zfq7vMQ"
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
     static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
