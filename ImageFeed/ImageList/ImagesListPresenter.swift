03bf2800a0523258a114e3e310f6087a2f1ecf99
Lastik85 <lastik198505@yandex.ru>
2025-10-27 18:28:37 +0300

    рефакторинг ImagesListViewController

---
+++ ImageFeed/ImageList/ImagesListPresenter.swift
@@ -0,0 +1,68 @@
+import Foundation
+
+final class ImagesListPresenter: ImagesListPresenterProtocol {
+    
+    weak var view: ImagesListControllerProtocol?
+    private let imagesService = ImagesListService.shared
+    var photos: [Photo] = []
+    
+    func viewDidLoad() {
+        fetchPhotosNextPage()
+        updateTableView()
+    }
+    
+    func getPhoto(for indexPath: IndexPath) -> Photo {
+        photos[indexPath.row]
+    }
+    
+    func photosCount() -> Int {
+        photos.count
+    }
+    
+    func fetchPhotosNextPage(){
+        imagesService.fetchPhotosNextPage() { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success:
+                    break
+                case .failure(let error):
+                    print("Ошибка загрузки фото: \(error)")
+                }
+            }
+        }
+    }
+    
+    func didTapLike(for indexPath: IndexPath, with cell: ImagesListCell){
+        let photo = photos[indexPath.row]
+        view?.showProgressHUD()
+        imagesService.changeLike(photoId: photo.id, isLike: !photo.isLiked) { [self] result in
+            switch result {
+            case .success:
+                DispatchQueue.main.async {
+                    if let index = self.photos.firstIndex(where: { $0.id == photo.id }) {
+                        self.photos[index].isLiked = !photo.isLiked
+                        cell.setIsLiked(!photo.isLiked)
+                    }
+                }
+                view?.hideProgressHUD()
+            case .failure:
+                view?.hideProgressHUD()
+            }
+        }
+    }
+    
+    func updateTableView() {
+        let oldCount = photos.count
+        let newCount = imagesService.photos.count
+        photos = imagesService.photos
+        if oldCount != newCount {
+            view?.updateTableViewAnimated(with: oldCount, newCount: newCount)
+        }
+    }
+    
+    func fetchNewPhotosPage(for indexPath: IndexPath) {
+        if indexPath.row + 1 == photos.count {
+            fetchPhotosNextPage()
+        }
+    }
+}
--- ImageFeed/TabBarController/TabBarController.swift
+++ ImageFeed/TabBarController/TabBarController.swift
@@ -1,20 +1,23 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         let storyboard = UIStoryboard(name: "Main", bundle: .main)
-
-        let imagesListViewController = storyboard.instantiateViewController(
-            withIdentifier: "ImagesListViewController"
-        )
+        guard let imagesListViewController = storyboard.instantiateViewController(withIdentifier: "ImagesListViewController") as? ImagesListViewController else {
+            assertionFailure("Failed to instantiate ImagesListViewController from Storyboard")
+            return
+        }
+        let imagesListPresenter = ImagesListPresenter()
+        imagesListViewController.configure(presenter: imagesListPresenter)
+        imagesListPresenter.view = imagesListViewController
         imagesListViewController.tabBarItem = UITabBarItem(
             title: "",
             image: UIImage(resource: .tabEditorialActive),
             selectedImage: nil
         )
-
+        
         let profileViewController = ProfileViewController()
         let profilePresenter = ProfileViewPresenter()
         profileViewController.presenter = profilePresenter
@@ -26,17 +29,17 @@ final class TabBarController: UITabBarController {
         )
         configureTabBar()
         viewControllers = [imagesListViewController, profileViewController]
-
+        
     }
-
+    
     private func configureTabBar() {
-
+        
         tabBar.barTintColor = UIColor(resource: .ypBlackIOS)
         tabBar.backgroundColor = UIColor(resource: .ypBlackIOS)
         tabBar.isTranslucent = false
         tabBar.tintColor = UIColor(resource: .ypWhiteIOS)
         tabBar.unselectedItemTintColor = UIColor(resource: .ypGrayIOS)
-
+        
     }
-
+    
 }
